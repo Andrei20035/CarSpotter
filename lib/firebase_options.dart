4eb9a5f52360672cbe2149f9fe5633469fdaf593
Andrei <amrusu2@gmail.com>
2024-07-04 19:23:13 +0300

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -48,21 +48,4 @@ class DefaultFirebaseOptions {
         );
     }
   }
-
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyD55Wi2qRuRWmDJnhdMs_5K1c9eMJc-C8E',
-    appId: '1:580518214015:android:b20a8584b4bbb08a3f824a',
-    messagingSenderId: '580518214015',
-    projectId: 'carspotter-9836c',
-    storageBucket: 'carspotter-9836c.appspot.com',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCWvf8Vgus-O2skJrWmN70RlOOF9RbR7mQ',
-    appId: '1:580518214015:ios:da805b4588829f5f3f824a',
-    messagingSenderId: '580518214015',
-    projectId: 'carspotter-9836c',
-    storageBucket: 'carspotter-9836c.appspot.com',
-    iosBundleId: 'com.example.carSpotter',
-  );
 }
